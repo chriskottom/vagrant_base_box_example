1923a75b1de5c612e64f323de0c21e9a02ad6301
Chris Kottom <chris@chriskottom.com>
2014-04-06 15:54:06 +0200

    basic cookbook to install required packages

---
+++ cookbooks/chriskottom_precise64/metadata.rb
@@ -0,0 +1,7 @@
+name             'chriskottom_precise64'
+maintainer       'Chris Kottom'
+maintainer_email 'chris@chriskottom.com'
+license          'MIT'
+description      'Installs packages for chriskottom_precise64 Vagrant base box'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
